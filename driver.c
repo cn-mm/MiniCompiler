be72df746413fc3f5d814467c60e8a0ff692d470
Mansi <mansimittal027@gmail.com>
2020-10-28 22:58:47 +0530

    Adding test cases

--- driver.c
+++ driver.c
@@ -4,24 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "parse.h"
-// int test(int argc, char const *argv[]){
+int test(int argc, char const *argv[]){
 
-//     int option;
+    int option;
 
-// 	do{
+	do{
 
-// 		printf("* \n Please Enter Option \n Input the option number corresponding to the desired input: \n\n");
+		printf("* \n Please Enter Option \n Input the option number corresponding to the desired input: \n\n");
 
-// 		printf("0: Exit\n");
-// 		printf("1: Create parse tree \n");
-// 		printf("2: Traverse the parse tree to construct typeExpressionTable \n");
-// 		printf("3: Print parse tree \n");
-// 		printf("4: Print typeExpressionTable \n");
-// 		scanf("%d", &option);
+		printf("0: Exit\n");
+		printf("1: Create parse tree \n");
+		printf("2: Traverse the parse tree to construct typeExpressionTable \n");
+		printf("3: Print parse tree \n");
+		printf("4: Print typeExpressionTable \n");
+		scanf("%d", &option);
 
-//         // Add functions corresponding to each option
-//     }
-//     while(option !=0);
+        // Add functions corresponding to each option
+    }
+    while(option !=0);
 
-//     return 0;
-// }
+    return 0;
+}
